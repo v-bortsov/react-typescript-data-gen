2ca8256b7febdefb4060c38a67989889cae027b3
Bortsov Vitalii <6622319@gmail.com>
2021-03-23 21:51:29 +0300

    fix: gen date

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,5 +1,5 @@
 import moment from 'moment'
-import { always, assoc, chain, clone, concat, converge, divide, flatten, length, multiply, negate, of, pair, pipe, prop, reduce, remove, repeat, slice, subtract } from 'ramda'
+import { always, assoc, chain, clone, concat, converge, curry, divide, flatten, head, ifElse, is, last, length, multiply, negate, of, pair, pipe, prop, reduce, remove, repeat, slice, subtract, tap, __ } from 'ramda'
 import { IDateOption, Interval } from '../react-app-env'
 
 // const opt = { days: [1, 2, 3], lengthDays: 7, limit: 10, mode: 'week|range', startDate: '', endDate: '' }
@@ -13,21 +13,34 @@ const ceilLimit = pipe(
   ), Math.ceil
 )
 
-export const addDaysToDate: any = (
+export const addDaysToDate: any = curry((
   currentDate: string, count: number, flag: Interval
 ) => moment(
   currentDate, 'DD.MM.YYYY'
 ).add(
   count, flag
 )
-  .format('DD.MM.YYYY')
-export const dayToDate = pipe<string[], any, any>(
+  .format('DD.MM.YYYY'))
+export const dayToDate = pipe<string[], any, any, any>(
   pair,
   converge(
-    concat, [prop<any>(0), pipe(
-      prop<any>(1), addDaysToDate, of
+    concat, [pipe(ifElse(
+      pipe(
+        prop<any>(0), is(String)
+      ), pipe(
+        prop<any>(0), of
+      ), prop<any>(0)
+    )), pipe( 
+      converge(
+        addDaysToDate(
+          __, __, 'days'
+        ), [pipe(
+          prop(0), last
+        ), prop<any>(1)]
+      ),
+      of
     )]
-  ) 
+  )
 )
 export const transformDates = pipe<any, any, any, IDateOption>(
   chain(
@@ -38,28 +51,28 @@ export const transformDates = pipe<any, any, any, IDateOption>(
       flatten
     )
   ),
-  chain(
-    assoc('template'),
-    converge(
-      slice, [always(0), pipe(
-        converge(
-          subtract, [converge(
-            multiply, [ceilLimit, countDays]
-          ), prop('limit')]
-        ), negate
-      ), clone]
-    )
-  ),
+  // tap(console.log),
+  // chain(
+  //   assoc('template'),
+  //   converge(
+  //     slice, [always(0), pipe(
+  //       converge(
+  //         subtract, [converge(
+  //           multiply, [ceilLimit, countDays]
+  //         ), prop('limit')]
+  //       ), negate
+  //     ), clone]
+  //   )
+  // ),
   chain(
     assoc('dates'),
     converge(
-      reduce, [dayToDate, pipe(
-        prop('startDate'), of
-      ), pipe(
-        prop('days'), remove(
+      reduce, [always(dayToDate), prop('startDate'), pipe(
+        prop('template'), remove(
           0, 1
         )
       )]
     )
-  )
+  ),
+  tap(console.log)
 )// (opt)
