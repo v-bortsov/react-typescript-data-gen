9a05f6c43d1b5a76c396d1db161381e13e9d8943
Your Name <you@example.com>
2021-03-24 20:47:03 +0300

    add transform days

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,9 +1,9 @@
 import moment from 'moment'
-import { always, assoc, chain, clone, concat, converge, curry, divide, flatten, head, ifElse, is, last, length, multiply, negate, of, pair, pipe, prop, reduce, remove, repeat, slice, subtract, tap, __ } from 'ramda'
+import { always, aperture, assoc, chain, clone, concat, converge, curry, divide, flatten, head, ifElse, is, last, length, multiply, negate, of, pair, pipe, prop, reduce, remove, repeat, slice, subtract, tap, __ } from 'ramda'
 import { IDateOption, Interval } from '../react-app-env'
 
 // const opt = { days: [1, 2, 3], lengthDays: 7, limit: 10, mode: 'week|range', startDate: '', endDate: '' }
-
+const interval = 7
 const countDays = pipe<any, number[], number>(
   prop('days'), length
 )
@@ -52,18 +52,36 @@ export const transformDates = pipe<any, any, any, IDateOption>(
     )
   ),
   // tap(console.log),
-  // chain(
-  //   assoc('template'),
-  //   converge(
-  //     slice, [always(0), pipe(
-  //       converge(
-  //         subtract, [converge(
-  //           multiply, [ceilLimit, countDays]
-  //         ), prop('limit')]
-  //       ), negate
-  //     ), clone]
+  // pipe(
+  //   aperture(2),
+  //   tap(console.log),
+  //   reduce(
+  //     (
+  //       acc: number[], curr: number[]
+  //     ) => { 
+  //       if (curr[1] < curr[0]) {
+  //         const calcL = (interval - curr[0]) + curr[1]
+  //         acc.push(calcL)
+  //       } else {
+  //         const calcG = curr[1] - curr[0]
+  //         acc.push(calcG)
+  //       }
+  //       return acc
+  //     }, []
   //   )
   // ),
+  chain(
+    assoc('template'),
+    converge(
+      slice, [always(0), pipe(
+        converge(
+          subtract, [converge(
+            multiply, [ceilLimit, countDays]
+          ), prop('limit')]
+        ), negate
+      ), clone]
+    )
+  ),
   chain(
     assoc('dates'),
     converge(
